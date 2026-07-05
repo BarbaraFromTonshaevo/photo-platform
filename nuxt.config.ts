02fda231986c04163f6585a6fd789bfec6e51164
Elkina Barbara <elkinavarvara1999@gmail.com>
2026-07-05 21:55:48 +0300

    fix [config]: add favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,22 @@ const root = new URL('.', import.meta.url).pathname
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  app: {
+    head: {
+        title: 'Фотограф Елькина Варвара',
+        meta: [
+            { name: 'description', content: 'Живые эмоции, естественный свет и внимание к деталям.' }
+        ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/logo-rotated.svg' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' }
+      ]
+    }
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
